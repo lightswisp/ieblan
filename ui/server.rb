90d85fd2addcdc3d7d38a827ae92e40af761c346
lightswisp <lightswisp@gmail.com>
2025-08-07 02:02:17 -0400

    changed gui a bit

--- ui/server.rb
+++ ui/server.rb
@@ -1,7 +1,9 @@
+require "json"
 require "sinatra"
 require 'sinatra/base'
 
 class Server < Sinatra::Base
+  @@appfilter = JSON.parse(File.read(File.join("ui", "appfilter.json")))
   if settings.development?
     set :bind, '0.0.0.0' 
     set :host_authorization, {
@@ -10,7 +12,7 @@ class Server < Sinatra::Base
   end
 
   get '/' do 
-    @apps = IO.popen(["cmd", "package", "list", "packages", "-U"]).read.split("\n")
+    @apps = IO.popen(["cmd", "package", "list", "packages", "-3", "-U"]).read.split("\n")
     erb :index
   end
 end
